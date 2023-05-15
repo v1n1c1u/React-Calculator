ff41d51d83eb7b7942de4cc258f12020ec187aee
v1n1c1u <vinicius.souza@ccc.ufcg.edu.br>
2023-05-15 15:06:02 -0300

    adding calculator skeleton and title

---
+++ src/main/calculator.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import './calculatorStyles.css';
+
+export default class Calculator extends Component {
+    render() {
+        return (
+            <div className="calculator">
+                <div className="screen">
+                </div>
+            </div>
+        )
+    }
+}
